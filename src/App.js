a8d762525bf8d49e8f568e1e8cfc20bef540c6c0
arslanaion <arsalan@aiondigital.com>
2020-06-22 04:05:31 +0500

    Update: App.js Added Listing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement, fetchPost } from "./actions";
+import post from "./reducers/posts";
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const isLoggedIn = useSelector((state) => state.isLogged);
+  const fetchedposts = useSelector((state) => state.posts);
   const dispatch = useDispatch();
   useEffect(() => {
     console.log("here");
@@ -16,8 +18,18 @@ function App() {
     <div className="App">
       <h1>Hello</h1>
       <h2>Counter:{counter}</h2>
+
       <button onClick={() => dispatch(increment())}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
+      {fetchedposts ? (
+        <ul>
+          {fetchedposts.map((item) => (
+            <li>{item.title}</li>
+          ))}
+        </ul>
+      ) : (
+        ""
+      )}
       {isLoggedIn ? <h1>here is confidential information</h1> : ""}
     </div>
   );
